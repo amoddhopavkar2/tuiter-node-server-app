1daf13737dc75af0a64bf292b6256807ec338111
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-03-28 20:29:00 -0400

    almost there

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-import express from "express";
-import HelloController from "./controllers/hello-controller.js";
-import UserController from "./controllers/users/users-controller.js";
+import express from 'express';
+import HelloController from './controllers/hello-controller.js';
+import UserController from './controllers/users/users-controller.js';
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-import cors from "cors";
+import cors from 'cors'
+import mongoose from "mongoose";
 
-const app = express();
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb+srv://amoddhopavkar2:<amod02021999>@cluster0.k4tv2xw.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(CONNECTION_STRING);
 
-app.use(cors());
+const app = express()
+
+app.use(cors())
 app.use(express.json());
 
-HelloController(app);
-UserController(app);
+HelloController(app)
+UserController(app)
 TuitsController(app);
 
-app.listen(process.env.PORT || 4000);
+app.get('/', (req, res) => { res.send('Welcome to Full Stack Development!') })
+app.listen(process.env.PORT || 4000)
